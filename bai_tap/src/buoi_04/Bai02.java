a3a19caff382655309bec75a1459ca4db9b42a7e
WuangNyH <kayzz0974@gmail.com>
2025-07-13 22:00:08 +0700

    bai tap string va date

--- bai_tap/src/buoi_04/Bai02.java
+++ bai_tap/src/buoi_04/Bai02.java
@@ -1,43 +1,35 @@
 package buoi_04;
 
 import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Bai02 {
     public static boolean checkPassword(String password) {
         password = password.trim();
 
         if (password.contains(" ")) {
-            System.out.println("Password không được để khoảng trắng!");
+            System.out.println("Mật khẩu không được chứa khoảng trắng!");
             return false;
         }
 
         if (password.length() < 8) {
-            System.out.println("Mật khẩu chứa ít nhất 8 ký tự!");
+            System.out.println("Mật khẩu phải chứa ít nhất 8 ký tự!");
             return false;
         }
 
-        int countLowerCase = 0;
-        int countUpperCase = 0;
-        int countSpecialCharacter = 0;
+        Pattern patternLowerCase = Pattern.compile("[a-z]");
+        Pattern patternUpperCase = Pattern.compile("[A-Z]");
+        Pattern patternNumber = Pattern.compile("\\d");
+        Pattern patternSymbol = Pattern.compile("[^0-9A-Za-z]");
 
-        for (int  i = 0; i < password.length(); i++) {
-            if (Character.isLowerCase(password.charAt(i))) {
-                countLowerCase++;
-            } else if (Character.isUpperCase(password.charAt(i))) {
-                countUpperCase++;
-            }  else if (!Character.isLetterOrDigit(password.charAt(i))) {
-                countSpecialCharacter++;
-            }
-        }
+        Matcher matcherLowerCase = patternLowerCase.matcher(password);
+        Matcher matcherUpperCase = patternUpperCase.matcher(password);
+        Matcher matcherNumber = patternNumber.matcher(password);
+        Matcher matcherSymbol = patternSymbol.matcher(password);
 
-        if  (countLowerCase == 0) {
-            System.out.println("Mật khẩu phải có ít nhất một chữ thường");
-            return false;
-        } else if (countUpperCase == 0) {
-            System.out.println("Mật khẩu phải có ít nhất một chữ hoa");
-            return false;
-        } else if (countSpecialCharacter == 0) {
-            System.out.println("Mật khẩu phải có ít nhất một ký tự đặt biệt");
+        if (!matcherLowerCase.find() ||  !matcherUpperCase.find() || !matcherNumber.find() || !matcherSymbol.find()) {
+            System.out.println("Mật khẩu phải có ít nhất 1 chữ hoa, 1 chữ thường, 1 chữ số và 1 ký tự đặc biệt!");
             return false;
         }
 
@@ -45,13 +37,12 @@ public class Bai02 {
     }
 
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+            Scanner sc = new Scanner(System.in);
+            System.out.print("Nhập mật khẩu của bạn: ");
+            String password = sc.nextLine();
 
-        System.out.print("Nhập mật khẩu của bạn: ");
-        String password = sc.nextLine();
-
-        if (checkPassword(password)) {
-            System.out.println("Mật khẩu mạnh!");
-        }
+            if (checkPassword(password)) {
+                System.out.println("Mật khẩu mạnh!");
+            }
     }
 }
