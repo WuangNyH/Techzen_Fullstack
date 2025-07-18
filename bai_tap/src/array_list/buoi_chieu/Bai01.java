b8e56b979cb59795ca5da8bd1928c353405b81fa
WuangNyH <kayzz0974@gmail.com>
2025-07-18 14:48:28 +0700

    bai tap array list

--- bai_tap/src/array_list/buoi_chieu/Bai01.java
+++ bai_tap/src/array_list/buoi_chieu/Bai01.java
@@ -1,8 +1,6 @@
 package array_list.buoi_chieu;
 
 import java.util.*;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 import static buoi_05.bai01_class.Bai01.validateData;
 
