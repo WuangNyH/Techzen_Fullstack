56a77dbd434bad0caff4b604cd79df9051efe61c
WuangNyH <kayzz0974@gmail.com>
2025-07-27 11:14:29 +0700

    bai tap nhom buoii 2

--- OOP/src/buoi_2/thuc_hanh/Student.java
+++ OOP/src/buoi_2/thuc_hanh/Student.java
@@ -1,5 +1,7 @@
 package buoi_2.thuc_hanh;
 
+import java.util.Scanner;
+
 public class Student {
     private int id;
     private String name;
@@ -10,6 +12,8 @@ public class Student {
     private static int countStudent = 0;
 
     public Student() {
+        this.id = autoId++;
+        countStudent++;
     }
 
     public Student(String name, int age, double avgScore) {
@@ -21,11 +25,64 @@ public class Student {
         countStudent++;
     }
 
+    public void input() {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Thêm sinh viên với id #" + (autoId - 1));
+
+        while (true) {
+            System.out.print("Nhập tên sinh viên: ");
+            this.name = sc.nextLine().trim();
+            if (this.name.matches("[a-zA-ZÀ-Ỹà-ỹ\\s]+")) break;
+            else System.out.println("Tên không hợp lệ!. Vui lòng nhập tên không chứa ký tự đặt biệt và chữ số");
+        }
+
+        while (true) {
+            System.out.print("Nhập tuổi: ");
+            if (sc.hasNextInt()) {
+                this.age = sc.nextInt();
+                if (this.age < 0) {
+                    System.out.println("Tuổi không hợp lệ!. Vui lòng nhập tuổi lớn hơn 0!");
+                    continue;
+                }
+                sc.nextLine();
+                break;
+            } else {
+                System.out.println("Tuổi không hợp lệ!. Vui lòng nhập tuổi lớn hơn 0!");
+                sc.nextLine();
+            }
+        }
+
+        while (true) {
+            System.out.print("Nhập điểm trung bình của sinh viên: ");
+            if (sc.hasNextFloat()) {
+                this.avgScore = sc.nextFloat();
+                if (this.avgScore < 0 || this.avgScore > 10) {
+                    System.out.println("Điểm không hợp lệ!. Vui lòng nhập điểm từ 0 - 10!");
+                    continue;
+                }
+                sc.nextLine();
+                break;
+            } else {
+                System.out.println("Điểm không hợp lệ!. Vui lòng nhập điểm từ 0 - 10!");
+                sc.nextLine();
+            }
+        }
+    }
+
     public String getName() {
         return name;
     }
 
     public double getAvgScore() {
         return avgScore;
+    }
+
+    public static int getCountStudent() {
+        return countStudent;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("%-5d | %-20s | %-5d | %-8.2f", id, name, age, avgScore);
     }
 }
--- OOP/src/buoi_2/thuc_hanh/StudentManager.java
+++ OOP/src/buoi_2/thuc_hanh/StudentManager.java
@@ -7,6 +7,82 @@ public class StudentManager {
     static ArrayList<Student> students = new ArrayList<>();
     static Scanner sc = new Scanner(System.in);
 
+    public static void addStudent() {
+        Student newStudent = new Student();
+        newStudent.input();
+        students.add(newStudent);
+        System.out.println("Thêm học viên thành công!\n");
+    }
+
+    public static void displayStudents(ArrayList<Student> students) {
+        System.out.println("============= Danh sách sinh viên =============");
+        System.out.printf("%-5s | %-20s | %-5s | %-8s\n",
+                "ID", "Tên", "Tuổi", "Điểm TB");
+
+        for (Student student : students) {
+            System.out.println(student);
+        }
+        System.out.println();
+    }
+
+    public static ArrayList<Student> findByName(String keyword) {
+        ArrayList<Student> list = new ArrayList<>();
+        if (isEmptyList()) {
+            return list;
+        }
+
+        for (Student student : students) {
+            if (student.getName().trim().toLowerCase().contains(keyword)) {
+                list.add(student);
+            }
+        }
+
+        return list;
+    }
+
+    private static boolean isEmptyList() {
+        if (students.isEmpty()) {
+            System.out.println("Chưa có học viên.\n");
+            return true;
+        }
+        return false;
+    }
+
+    private static ArrayList<Student> findStudentMaxScore() {
+        ArrayList<Student> result = new ArrayList<>();
+        if (isEmptyList()) {
+            return result;
+        }
+
+        double maxScore = students.get(0).getAvgScore();
+        for (Student s : students) {
+            if (s.getAvgScore() > maxScore) {
+                maxScore = s.getAvgScore();
+                result.clear();
+                result.add(s);
+            } else if (s.getAvgScore() == maxScore) {
+                result.add(s);
+            }
+        }
+
+        return result;
+    }
+
+    private static ArrayList<Student> findStudentAvgScore() {
+        ArrayList<Student> result = new ArrayList<>();
+        if (isEmptyList()) {
+            return result;
+        }
+
+        for (Student s : students) {
+            if (s.getAvgScore() < 5) {
+                result.add(s);
+            }
+        }
+        return result;
+    }
+
+
     public static void displayMenu() {
         System.out.println(">> LỰA CHỌN CHỨC NĂNG <<");
         System.out.println("++ ----------------------------------------- ++");
@@ -16,7 +92,7 @@ public class StudentManager {
         System.out.println("| 4. Hiện thị tổng số sinh viên               |");
         System.out.println("| 5. Sinh viên có điểm cao nhất               |");
         System.out.println("| 6. Sinh viên có điểm dưới trung bình ( < 5) |");
-        System.out.println("| 7. Exit                                     |");
+        System.out.println("| 7. Thoát chương trình                       |");
         System.out.println("++ ----------------------------------------- ++");
     }
 
@@ -28,18 +104,48 @@ public class StudentManager {
 
             switch (choice) {
                 case 1:
+                    addStudent();
                     break;
                 case 2:
+                    if (isEmptyList()) {
+                        break;
+                    }
+                    displayStudents(students);
                     break;
                 case 3:
+                    System.out.print("Nhập tên cần tìm: ");
+                    String keyword = sc.nextLine().trim().toLowerCase();
+                    ArrayList<Student> listByName = findByName(keyword);
+
+                    if (listByName.isEmpty()) {
+                        System.out.println("Không tìm thấy sinh viên nào tên là " + keyword + "\n");
+                        break;
+                    }
+                    displayStudents(listByName);
                     break;
                 case 4:
+                    System.out.println("Tổng sinh viên trong danh sách là " + Student.getCountStudent());
                     break;
                 case 5:
+                    ArrayList<Student> studentsMaxScore = findStudentMaxScore();
+                    if (studentsMaxScore.isEmpty()) {
+                        break;
+                    }
+                    displayStudents(studentsMaxScore);
                     break;
                 case 6:
+                    ArrayList<Student> studentsAvgScore = findStudentAvgScore();
+                    if (studentsAvgScore.isEmpty()) {
+                        break;
+                    }
+                    displayStudents(studentsAvgScore);
                     break;
                 default:
+                    System.out.print("Chắc chắn muốn thoát chương trình (C/K): ");
+                    String exit = sc.nextLine();
+                    if (exit.equalsIgnoreCase("c")) {
+                        return;
+                    }
                     break;
             }
         }
