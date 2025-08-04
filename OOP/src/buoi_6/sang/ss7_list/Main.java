edd2d4268eaa4e6ab81091e3967503efa3a66f60
WuangNyH <kayzz0974@gmail.com>
2025-08-04 13:04:06 +0700

    bai tap truu tuong

---
+++ OOP/src/buoi_6/sang/ss7_list/Main.java
@@ -0,0 +1,33 @@
+package buoi_6.sang.ss7_list;
+
+
+public class Main {
+    public static void main(String[] args) {
+//        MyArrayList myArrayList = new MyArrayList();
+//        myArrayList.add(10);
+//        myArrayList.add(20);
+//        myArrayList.add(30);
+//        myArrayList.add(40);
+//
+//        System.out.println(myArrayList);
+
+        MyLinkedList myLinkedList = new MyLinkedList();
+        myLinkedList.addFirst(10);
+        myLinkedList.addFirst(20);
+        myLinkedList.addFirst(30);
+
+//        myLinkedList.addLast(40);
+//        myLinkedList.addLast(50);
+//
+//        myLinkedList.add(2, 90);
+//        myLinkedList.add(3, 91);
+//
+//        System.out.println(myLinkedList.removeFirst());
+//
+//        System.out.println(myLinkedList.removeLast());
+
+        System.out.println(myLinkedList.remove(2));
+
+        System.out.println(myLinkedList);
+    }
+}
